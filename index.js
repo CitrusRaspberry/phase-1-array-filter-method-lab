e93d61a0d37023551dcb74fe5d095c976041c575
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-11-04 00:28:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter( driver => driver.toLowerCase() === name.toLowerCase() )
+}
+
+function fuzzyMatch(drivers, search) {
+  return drivers.filter( driver => driver.indexOf(search) === 0 );
+}
+
+function matchName(drivers, name) {
+  return drivers.filter( driver => driver.name === name );
+}
